4a0aec6296a86c712834a6155a99833f77cfb434
andrey <andrey28059999@gmail.com>
2023-03-07 12:44:26 +0300

    refactor: add environment variables

--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
-GROUPS_COUNT = 10
-STUDENTS_IN_GROUP_COUNT = 30
-TYPE_REPORT = "json"
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+GROUPS_COUNT = int(os.getenv("GROUPS_COUNT"))
+STUDENTS_IN_GROUP_COUNT = int(os.getenv("STUDENTS_IN_GROUP_COUNT"))
+TYPE_REPORT = os.getenv("TYPE_REPORT")
