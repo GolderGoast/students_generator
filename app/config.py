10f460f6404b4de9a81fd5c0364d34777d35f2a2
andrey <andrey28059999@gmail.com>
2023-03-25 12:21:56 +0300

    refactor: add env from docker compose

--- app/config.py
+++ app/config.py
@@ -7,12 +7,12 @@ class Settings(BaseSettings):
     groups_count: int = 10
     students_in_group_count: int = 30
     type_report: str = "db"
-    scheme_db: str = "postgresql"
-    user_db: str = "postgres"
-    password_db: str = "postgres"
-    name_db: str = "reports"
-    host_db: str = "localhost"
-    port_db: str = "5432"
+    postgres_scheme: str = "postgresql"
+    postgres_user: str = "postgres"
+    postgres_password: str = "postgres"
+    postgres_db: str = "reports"
+    postgres_host: str = "localhost"
+    postgres_port: str = "5432"
     dsn: str | None
 
     @validator("dsn", pre=True)
@@ -21,16 +21,13 @@ class Settings(BaseSettings):
             return value
 
         return PostgresDsn.build(
-            scheme=values["scheme_db"],
-            host=values["host_db"],
-            port=values["port_db"],
-            user=values["user_db"],
-            password=values["password_db"],
-            path=f"/{values['name_db']}",
+            scheme=values["postgres_scheme"],
+            host=values["postgres_host"],
+            port=values["postgres_port"],
+            user=values["postgres_user"],
+            password=values["postgres_password"],
+            path=f"/{values['postgres_db']}",
         )
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
