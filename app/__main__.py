7a17302021627ae9118b03f0facd5c601ff7b82b
andrey <andrey28059999@gmail.com>
2023-03-29 15:38:02 +0300

    feat: add flask

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,6 @@
 import typer
 from faker import Faker
+from flask import Flask, jsonify, request
 
 from config import GROUPS_COUNT, STUDENTS_IN_GROUP_COUNT, TYPE_REPORT
 from domain.create_group_student import FakeGroupData, FakeStudentData, Group, GroupCreator, StudentCreator
@@ -11,19 +12,27 @@ from repositories.reports.json_report import JsonReport
 from repositories.reports.pdf_report import PDFReport
 from repositories.reports.xlsx_report import XLSXReport
 
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
+
 REPORT_TYPES = {"xlsx": XLSXReport, "json": JsonReport, "pdf": PDFReport, "db": DataBaseReport}
 
-app = typer.Typer()
+typer_app = typer.Typer()
 
 
-@app.command()
-def main(gc: int = GROUPS_COUNT, sc: int = STUDENTS_IN_GROUP_COUNT, rtype: str = TYPE_REPORT) -> None:
+@app.route("/report")
+@typer_app.command()
+def main(gc: int = GROUPS_COUNT, sc: int = STUDENTS_IN_GROUP_COUNT, rtype: str = TYPE_REPORT) -> jsonify:
     fake_student_data = FakeStudentData(faker=Faker("ru_RU"))
     fake_group_data = FakeGroupData(faker=Faker("ru_RU"))
 
     group_creator = GroupCreator(faker=fake_group_data)
     student_creator = StudentCreator(faker=fake_student_data)
     timetable_creator = TimeTableCreator()
+
+    gc = int(request.args.get("gc")) if request.args.get("gc") else gc
+    sc = int(request.args.get("sc")) if request.args.get("sc") else sc
+    rtype = request.args.get("rtype") if request.args.get("rtype") else rtype
 
     builder = GroupsBuilder(
         group_creator=group_creator,
@@ -37,6 +46,8 @@ def main(gc: int = GROUPS_COUNT, sc: int = STUDENTS_IN_GROUP_COUNT, rtype: str =
     report: IReportGetter = REPORT_TYPES[rtype](groups)
     report.get_report()
 
+    return jsonify({"message": "Отчет готов"})
+
 
 if __name__ == "__main__":
-    app()
+    app.run(debug=True, host="0.0.0.0")
