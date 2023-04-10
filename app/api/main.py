aaaea0b99b6d4683980f2f0c997ee9e85a54fe9e
andrey <andrey28059999@gmail.com>
2023-04-10 16:34:53 +0300

    refactor: add dir api

--- app/api/main.py
+++ app/api/main.py
@@ -1,5 +1,5 @@
-import typer
 from faker import Faker
+from flask import Flask, jsonify, request
 
 from config import GROUPS_COUNT, STUDENTS_IN_GROUP_COUNT, TYPE_REPORT
 from domain.create_group_student import FakeGroupData, FakeStudentData, Group, GroupCreator, StudentCreator
@@ -8,17 +8,22 @@ from domain.group_builder import GroupsBuilder
 from domain.report_creator import IReportGetter
 from repositories.reports.types import REPORT_TYPES
 
-app = typer.Typer()
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
 
 
-@app.command()
-def main(gc: int = GROUPS_COUNT, sc: int = STUDENTS_IN_GROUP_COUNT, rtype: str = TYPE_REPORT) -> None:
+@app.route("/report")
+def main(gc: int = GROUPS_COUNT, sc: int = STUDENTS_IN_GROUP_COUNT, rtype: str = TYPE_REPORT) -> jsonify:
     fake_student_data = FakeStudentData(faker=Faker("ru_RU"))
     fake_group_data = FakeGroupData(faker=Faker("ru_RU"))
 
     group_creator = GroupCreator(faker=fake_group_data)
     student_creator = StudentCreator(faker=fake_student_data)
     timetable_creator = TimeTableCreator()
+
+    gc = request.args.get("gc", default=gc, type=int)
+    sc = request.args.get("sc", default=sc, type=int)
+    rtype = request.args.get("rtype", default=rtype, type=str)
 
     builder = GroupsBuilder(
         group_creator=group_creator,
@@ -32,6 +37,8 @@ def main(gc: int = GROUPS_COUNT, sc: int = STUDENTS_IN_GROUP_COUNT, rtype: str =
     report: IReportGetter = REPORT_TYPES[rtype](groups)
     report.get_report()
 
+    return jsonify({"message": "Отчет готов"})
+
 
 if __name__ == "__main__":
-    app()
+    app.run(debug=True, host="0.0.0.0")
